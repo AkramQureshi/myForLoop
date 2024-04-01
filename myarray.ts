831b03a27fde2dbfcc1e82ea3a4d3b01781d23d3
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-04-01 23:10:29 +0500

    Add files via upload

---
+++ myarray.ts
@@ -0,0 +1,27 @@
+//List of fruit Names by using Arrays
+let fruitNames=["banana", "apple", "orange", "peach"]
+console.log("These are the Fruit Names:", fruitNames)
+
+//add more fruit names by using .push()
+fruitNames.push("melon", "watermelon", "strawberry")
+console.log("Added More Fruit Names:", fruitNames)
+
+//List of Vegitable Names by using Arrays
+let vegiNames=["Karela", "Loki", "Tori", "Palak"]
+console.log("These are the Vegitables:", vegiNames)
+
+//add more vegitable names by using .push()
+vegiNames.push("Potato", "Tomato", "Cacumber")
+console.log("Added More Vegitables:", vegiNames)
+
+
+//delete fruit names by using .pop() (will delete a last item from the array list)
+fruitNames.pop()
+console.log("These are the Fruitnames after deletion of last item:", fruitNames)
+
+//delete fruit names by using .pop() (will delete a last item from the array list)
+vegiNames.pop()
+console.log("These are the Vegitable names after deletion the last item:", vegiNames)
+
+
+//console.log("Fruits:", fruitNames, "Vegitables:", vegiNames)
